3a4c35b5f88a4ee65ffc8353d00b4fd6a3746bc8
Krishanonymous <sri.srikrishnan123@gmail.com>
2023-03-05 16:05:08 +0530

    prod go

--- ecell_web/settings.py
+++ ecell_web/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x8%=wo*h2(sz5b0)%!3g8uzst!2z+)*eu7o3tapqk)q3sp8_l0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
